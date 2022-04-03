feab9955d5d9a0f24fb9cee8073143fff4b8b6d6
mariosknl <mariosknl@outlook.com>
2022-04-03 10:41:50 +0300

    adding icons to the bottom tabs

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -1,3 +1,4 @@
+import { Entypo, FontAwesome } from "@expo/vector-icons";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -7,16 +8,22 @@ import PlannerScreen from "../screens/PlannerScreen";
 const Stack = createNativeStackNavigator();
 
 export default function Navigation() {
-  return <RootNavigator />;
+  return (
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
+  );
 }
 
 function RootNavigator() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Root" component={BottomTabNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}
+    >
+      <Stack.Screen name="Root" component={BottomTabNavigator} />
+    </Stack.Navigator>
   );
 }
 
@@ -25,8 +32,24 @@ const BottomTab = createBottomTabNavigator();
 function BottomTabNavigator() {
   return (
     <BottomTab.Navigator initialRouteName="Home">
-      <BottomTab.Screen name="Home" component={HomeScreen} />
-      <BottomTab.Screen name="Planner" component={PlannerScreen} />
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <FontAwesome name="home" size={size} color={color} />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Planner"
+        component={PlannerScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="add-to-list" size={size} color={color} />
+          ),
+        }}
+      />
     </BottomTab.Navigator>
   );
 }
