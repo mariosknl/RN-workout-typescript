f4397c64b1fe4b7d000adbfcdc3e0d770538389b
mariosknl <mariosknl@outlook.com>
2022-04-03 09:08:59 +0300

    installing react navigation

---
+++ screens/PlannerScreen.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+const PlannerScreen = () => {
+  return (
+    <View>
+      <Text>PlannerScreen</Text>
+    </View>
+  );
+};
+
+export default PlannerScreen;
+
+const styles = StyleSheet.create({});
