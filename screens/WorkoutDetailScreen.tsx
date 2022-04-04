23989298eaa637fa19be973cb13d94aea5d93668
mariosknl <mariosknl@outlook.com>
2022-04-04 11:19:07 +0300

    finishing modal

--- screens/WorkoutDetailScreen.tsx
+++ screens/WorkoutDetailScreen.tsx
@@ -3,6 +3,7 @@ import { NativeStackHeaderProps } from "@react-navigation/native-stack";
 import { StyleSheet, Text, View } from "react-native";
 import { Modal } from "../components/styled/Modal";
 import { PressableText } from "../components/styled/PressableText";
+import WorkoutItem from "../components/WorkoutItem";
 import { useWorkoutBySlug } from "../hooks/useWorkoutBySlug";
 import { formatSec } from "../utils/time";
 
@@ -21,26 +22,30 @@ const WorkoutDetailScreen = ({ route }: Navigation) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.header}>Slug - {workout?.name}</Text>
-      <Modal
-        activator={({ handleOpen }) => (
-          <PressableText text="Check Sequence" onPress={handleOpen} />
-        )}
-      >
-        <View>
-          {workout?.sequence.map((exercise, index) => (
-            <View key={index} style={styles.sequenceItem}>
-              <Text>
-                {exercise.name} | {exercise.type} |{" "}
-                {formatSec(exercise.duration)}
-              </Text>
-              {index !== workout.sequence.length - 1 && (
-                <FontAwesome name="arrow-down" size={20} />
-              )}
-            </View>
-          ))}
-        </View>
-      </Modal>
+      <WorkoutItem item={workout} childStyles={{ marginTop: 10 }}>
+        <Modal
+          activator={({ handleOpen }) => (
+            <PressableText text="Check Sequence" onPress={handleOpen} />
+          )}
+        >
+          <View>
+            {workout?.sequence.map((exercise, index) => (
+              <View key={index} style={styles.sequenceItem}>
+                <Text>
+                  {exercise.name} | {exercise.type} |{" "}
+                  {formatSec(exercise.duration)}
+                </Text>
+                {index !== workout.sequence.length - 1 && (
+                  <FontAwesome name="arrow-down" size={20} />
+                )}
+              </View>
+            ))}
+          </View>
+        </Modal>
+      </WorkoutItem>
+      <View>
+        <FontAwesome name="play-circle-o" size={100} />
+      </View>
     </View>
   );
 };
